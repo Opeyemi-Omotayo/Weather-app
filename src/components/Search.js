5a13c965876ec0075ebb341ca4f226b603d7953c
Opeyemi-Omotayo <Opeyemi.Motayo1@gmail.com>
2023-08-10 20:29:31 +0100

    search functionality

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Search = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const searchTermHandler = (e) => {
+        e.preventDefault();
+        setSearchTerm(e.target.value);
+    }
+    console.log(searchTerm);
   return (
-    <div class="flex items-center justify-center my-4">
-  <form class=" flex items-center justify-center flex-col sm:flex-row w-4/5">
+    <div className="flex items-center justify-center my-4">
+  <form className=" flex items-center justify-center flex-col sm:flex-row w-4/5">
     <input
       type="text"
-      placeholder="type your city..."
-      class="py-2 px-4 border border-gray-300 rounded-md mb-2 sm:mb-0 sm:mr-2 sm:w-1/2 focus:ring focus:ring-opacity-50 focus:border-gray-800 outline-none"
+      placeholder="search city..."
+      className="py-2 px-4 border border-gray-300 rounded-md mb-2 sm:mb-0 sm:mr-2 sm:w-1/2 focus:ring focus:ring-opacity-50 focus:border-gray-800 outline-none"
+      value={searchTerm}
+      onChange={searchTermHandler}
     />
     <button
       type="submit"
-      class="bg-gray-900 text-white py-2 px-4 rounded-md sm:w-auto hover:bg-gray-800 focus:ring focus:ring-opacity-50 focus:outline-none"
+      className="bg-gray-900 text-white py-2 px-4 rounded-md sm:w-auto hover:bg-gray-800 focus:ring focus:ring-opacity-50 focus:outline-none"
     >
       Search
     </button>
